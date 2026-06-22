43465e442889cd1f8ffa0785ef2a2a7d3d57d27f
Burvye <thebuffonion@gmail.com>
2026-06-22 02:15:36 -0400

    build: prepare webtransport frontend dependencies

--- src/realtime/client.rs
+++ src/realtime/client.rs
@@ -1,3 +1,11 @@
+use js_sys::{Reflect, Uint8Array};
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+use web_sys::{
+    ReadableStream, ReadableStreamDefaultReader
+};
+
+
 pub async fn connect() {}
 
 pub struct Realtime {}
