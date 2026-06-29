5a35a5ff6103c45c2352658d77c67ee5041b87cc
Burvye <thebuffonion@gmail.com>
2026-06-29 14:10:08 -0400

    feat: add content to input handling for the main player

--- game/src/player/define.rs
+++ game/src/player/define.rs
@@ -4,8 +4,13 @@ use bevy_tnua::prelude::*;
 
 #[derive(Component, Clone, Default)]
 pub struct PlayerData {
+    /// the velocity a player wants to go
+    /// should be reset every frame
     pos: Vec3,
 }
+
+#[derive(Component)]
+pub struct MainPlayer;
 
 #[derive(TnuaScheme)]
 #[scheme(basis = TnuaBuiltinWalk)]
@@ -17,8 +22,13 @@ pub enum PlayerScheme {
 pub fn build_scene(mut cmds: Commands) {
     let entity = cmds.spawn_scene(spawn_player()).id();
     // add additional things here the legacy way
-    cmds.entity(entity)
-        .insert((RigidBody::Dynamic, Collider::capsule(0.5, 1.0)));
+    cmds.entity(entity).insert((
+        RigidBody::Dynamic,
+        Collider::capsule(0.5, 1.0),
+        TnuaController::<PlayerScheme>::default(),
+        TnuaConfig::<PlayerScheme>(config_handle),
+        // playerdata handled in bsn macro
+    ));
 }
 /// put bsn macro stuff in here and unfortunately it is not fully supported
 /// so stuff like avian 3d stuff and wahtever put in the build scene additional inserts
--- game/src/player/input.rs
+++ game/src/player/input.rs
@@ -1,1 +1,34 @@
+use bevy::prelude::*;
+use bevy_tnua::prelude::*;
 
+const SPEED: f32 = 10.0;
+
+pub fn move_player(
+    mut player: Single<
+        (
+            &mut TnuaController<super::define::PlayerScheme>,
+            &mut super::define::PlayerData,
+        ),
+        With<super::define::MainPlayer>,
+    >,
+    keys: Res<ButtonInput<KeyCode>>,
+) {
+    let mut desire: Vec3 = Vec3::ZERO;
+    // TODO: replace with configurable keybinds later on
+    if keys.pressed(KeyCode::KeyW) {
+        desire.z += SPEED;
+    }
+    if keys.pressed(KeyCode::KeyS) {
+        desire.z -= SPEED;
+    }
+    if keys.pressed(KeyCode::KeyA) {
+        desire.x -= SPEED;
+    }
+    if keys.pressed(KeyCode::KeyD) {
+        desire.x += SPEED;
+    }
+    player.basis = TnuaBuiltinWalk {
+        desired_motion: desire,
+        desired_forward: todo!(),
+    }
+}
