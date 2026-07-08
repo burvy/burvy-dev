9845b83a16c480d77ece6513b57df6c80742150c
Burvye <thebuffonion@gmail.com>
2026-07-08 14:19:21 -0400

    feat: test site stuff

--- src/experiences/test.rs
+++ src/experiences/test.rs
@@ -2,5 +2,12 @@ use leptos::prelude::*;
 
 #[component]
 pub fn Test() -> impl IntoView {
-    view! { <p>"wow test site works"</p> }
+    let (toggle, set_toggle) = signal(false);
+    view! {
+        <div>
+            <button on:click=move |_| {
+                set_toggle.set(!toggle.get())
+            }>{move || toggle.get()}</button>
+        </div>
+    }
 }
