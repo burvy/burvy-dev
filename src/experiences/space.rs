b6ddfdd53321704db951a2fea5daf1dd549038a3
Burvye <thebuffonion@gmail.com>
2026-07-08 20:00:16 -0400

    feat: space picture!

--- src/experiences/space.rs
+++ src/experiences/space.rs
@@ -2,5 +2,11 @@ use leptos::prelude::*;
 
 #[component]
 pub fn SpacePhotos() -> impl IntoView {
-    view! { <p>"test"</p> }
+    view! {
+        <button>"Click me for a space picture!"</button>
+        <img
+            src="https://apod.nasa.gov/apod/image/2607/NGC6769LRGBcropAZ-1500-20-May-2026-1024.jpg"
+            alt="image description"
+        />
+    }
 }
