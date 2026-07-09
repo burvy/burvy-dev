6b84494bc99ff9139f1e3fb2c838630761b8c6ad
Burvye <thebuffonion@gmail.com>
2026-07-08 20:02:30 -0400

    feat: wrap in div

--- src/experiences/space.rs
+++ src/experiences/space.rs
@@ -3,10 +3,15 @@ use leptos::prelude::*;
 #[component]
 pub fn SpacePhotos() -> impl IntoView {
     view! {
-        <button>"Click me for a space picture!"</button>
-        <img
-            src="https://apod.nasa.gov/apod/image/2607/NGC6769LRGBcropAZ-1500-20-May-2026-1024.jpg"
-            alt="image description"
-        />
+        <div>
+            <button>"Click me for a space picture!"</button>
+        </div>
+        // div puts it on a new line.
+        <div>
+            <img
+                src="https://apod.nasa.gov/apod/image/2607/NGC6769LRGBcropAZ-1500-20-May-2026-1024.jpg"
+                alt="image description"
+            />
+        </div>
     }
 }
