b21b6ad1715698b11fe9cb84481bb514a602d80e
Burvye <thebuffonion@gmail.com>
2026-08-31 01:25:17 -0400

    feat: soundboard styling

--- src/experiences/soundboard.rs
+++ src/experiences/soundboard.rs
@@ -4,17 +4,35 @@ use leptos::prelude::*;
 /// a list of all the sounds
 /// just increment the list size and add more
 /// sounds onto it
-const SOUNDS: [&str; 3] = [
-    "sounds/ding.mp3",
-    "sounds/question.mp3",
-    "sounds/aria-math.mp3",
+const SOUNDS: [SoundInfo; 3] = [
+    SoundInfo {
+        path: "sounds/ding.mp3",
+        name: "🔔",
+        background: "images/ding.png",
+    },
+    SoundInfo {
+        path: "sounds/question.mp3",
+        name: "❓",
+        background: "images/question.png",
+    },
+    SoundInfo {
+        path: "sounds/aria-math.mp3",
+        name: "Old",
+        background: "images/aria-math.png",
+    },
 ];
+
+#[derive(Copy, Clone)]
+pub struct SoundInfo {
+    pub path: &'static str,
+    pub name: &'static str,
+    pub background: &'static str,
+}
 
 #[component]
 pub fn Soundboard() -> impl IntoView {
     view! {
-        <p>
-            "test"
+        <p class="card-list">
             {SOUNDS
                 .iter()
                 .copied()
@@ -29,8 +47,8 @@ pub fn Soundboard() -> impl IntoView {
 /// repurposing the experience card to make sound cards
 /// TODO: keep going
 #[component]
-pub fn SoundCard(sound: &'static str) -> impl IntoView {
-    let play_sound = sound::play_sound(sound);
+pub fn SoundCard(sound: SoundInfo) -> impl IntoView {
+    let play_sound = sound::play_sound(sound.path);
     view! {
         <div
             // instance 2 of using the sound::play_sound api
@@ -39,8 +57,9 @@ pub fn SoundCard(sound: &'static str) -> impl IntoView {
                 play_sound();
             }
             class="card"
+            style=format!("background-image: url('{}')", sound.background)
         >
-            <h1>{sound}</h1>
+            <h1>{sound.name}</h1>
         </div>
     }
 }
