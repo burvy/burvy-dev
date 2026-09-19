a98f01ee8cec7e7b977dc08806a7ce4ca3aa0ba6
Burvye <thebuffonion@gmail.com>
2026-09-19 12:40:01 -0400

    feat: venture

--- src/experiences/venture.rs
+++ src/experiences/venture.rs
@@ -6,17 +6,13 @@ use crate::lazy;
 pub fn Venture() -> impl IntoView {
     let canvas = NodeRef::<leptos::html::Canvas>::new();
 
-    Effect::new(move |_| {
-        if canvas.get().is_some() {
+    Effect::new(|_| {
+        request_animation_frame(|| {
             lazy::start_experience("/venture/venture-wasm.js");
-        }
+        });
     });
 
     view! {
-        <div id="game-wrapper">
-            <canvas node_ref=canvas id="venture-canvas">
-                "Loading..."
-            </canvas>
-        </div>
+        <div id="game-wrapper"></div>
     }
 }
